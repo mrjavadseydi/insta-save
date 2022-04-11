dd2bd66bb41b012a1c1653f61fa54b0a0e6dc8dc
mohammad javad seydi <seydi.birjand@gmail.com>
2022-04-11 12:44:02 +0430

    login and get coockie

--- app/Jobs/LoginJob.php
+++ app/Jobs/LoginJob.php
@@ -2,26 +2,34 @@
 
 namespace App\Jobs;
 
+use App\Models\Member;
+use App\Models\Page;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Http;
 
 class LoginJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $chat_id, $username, $password;
 
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($chat_id, $username, $password)
     {
-        //
+        $this->chat_id = $chat_id;
+        $this->username = $username;
+        $this->password = $password;
     }
+
 
     /**
      * Execute the job.
@@ -30,6 +38,35 @@ class LoginJob implements ShouldQueue
      */
     public function handle()
     {
-        //
+        $request = Http::get('http://194.5.192.39:8000/auth/login', [
+            'username' => $this->username,
+            'password' => $this->password,
+        ]);
+        $body = $request->body();
+        if (strpos($body, '}') !== false) {
+            $json = json_decode($body, true);
+            $text = 'خطایی رخ داده است لطفا مجددا تلاش کنید';
+            if (isset($json['exc_type'])) {
+                if ($json['exc_type'] == 'BadPassword') {
+                    $text = 'نام کاربری یا رمز عبور اشتباه است';
+                }
+                sendMessage([
+                    'chat_id' => $this->chat_id,
+                    'text' => $text,
+                ]);
+
+            } else {
+                sendMessage([
+                    'chat_id' => $this->chat_id,
+                    'text' => 'ورود موفقیت آمیز بود ! ',
+                ]);
+                Page::create([
+                    'member_id' => Member::where('chat_id', $this->chat_id)->first()->id,
+                    'username' => $this->username,
+                    'password' => $this->password,
+                    'coockie' => $body,
+                ]);
+            }
+        }
     }
 }
