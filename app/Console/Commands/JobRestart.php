be6ee2c98b938d4899d9abc461b98be5ae14f514
mohammad javad seydi <seydi.birjand@gmail.com>
2022-04-23 19:08:37 +0430

    optimization

---
+++ app/Console/Commands/JobRestart.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class JobRestart extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'job:restart';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        for ($i =0 ; $i<20; $i++) {
+            Artisan::call('queue:retry all');
+
+            sleep(5);
+        }
+        return 0;
+    }
+}
--- app/Lib/Interfaces/TelegramVarables.php
+++ app/Lib/Interfaces/TelegramVarables.php
@@ -1,35 +1,42 @@
 <?php
+
 namespace App\Lib\Interfaces;
+
 use App\Models\Member;
+use Illuminate\Support\Facades\Cache;
 
 abstract class TelegramVarables
 {
-    public $message_type,$data,$text,$chat_id,$from_id, $chat_type,$update;
+    public $message_type, $data, $text, $chat_id, $from_id, $chat_type, $update;
     public $user = null;
 
     public function __construct($update)
     {
         $this->message_type = messageType($update);
         $this->update = $update;
-        if ($this->message_type=="callback_query"){
+        if ($this->message_type == "callback_query") {
             $this->data = $update["callback_query"]['data'];
             $this->chat_id = $update["callback_query"]['message']['chat']['id'];
             $this->from_id = $update["callback_query"]['message']['chat']['id'];
             $this->message_id = $update["callback_query"]["message"]['message_id'];
             $this->text = $update["callback_query"]['message']['text'];
             $this->type = $update["callback_query"]['message']['chat']['type'];
-        }else{
+        } else {
             $this->text = $update['message']['text'] ?? "//**";
             $this->chat_id = $update['message']['chat']['id'] ?? "";
             $this->from_id = $update['message']['from']['id'] ?? "";
             $this->type = $update['message']['chat']['type'] ?? "";
         }
-        $this->user= Member::query()->firstOrCreate(['chat_id'=>$this->from_id],[
-            'state'=>null,
-            'first_name'=>$update['message']['from']['first_name']??null,
-            'last_name'=>$update['message']['from']['last_name']??null,
-            'username'=>$update['message']['from']['username']??null,
-            'block'=>0,
-        ]);
+        $chat_id = $this->chat_id;
+        $this->user =
+            Cache::remember('user_laravel' . $this->chat_id, 50, function () use ($update,$chat_id) {
+                return Member::query()->firstOrCreate(['chat_id' => $chat_id], [
+                    'state' => null,
+                    'first_name' => $update['message']['from']['first_name'] ?? null,
+                    'last_name' => $update['message']['from']['last_name'] ?? null,
+                    'username' => $update['message']['from']['username'] ?? null,
+                    'block' => 0,
+                ]);
+            });
     }
 }
